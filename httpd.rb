d01126b46661a2c225e89957358b0ba6453d71e4
Petre Andrei <p31andrei (at) gmail.com>
2012-03-04 12:47:23 +0200

    Can now add multiple commands. Exit at two enters.

--- httpd.rb
+++ httpd.rb
@@ -12,6 +12,16 @@ def getResponseFromClient(client)
     first_line = client.gets
     verb, path = first_line.split
 
+    if verb == nil and path == nil
+        first_line = client.gets
+        verb, path = first_line.split
+
+        # we exit, as no valid input was given
+        if verb == nil and path == nil
+            return nil, nil
+        end
+    end
+
     data = ""
     # check that verb is GET
     if verb != 'GET'
@@ -46,17 +56,22 @@ end
 if __FILE__ == $0
     httpd = TCPServer.open("0.0.0.0", 6000)
     client = httpd.accept
-    loop do
-        # header
-        client.puts "HTTP/1.0 200 OK"
-        client.puts ""
 
+    # header
+    client.puts "HTTP/1.0 200 OK"
+    client.puts ""
+
+    loop do
         # get input from client
         header, data = getResponseFromClient(client)
-        client.puts header
-        client.puts data
-        break
+        if header == nil and data == nil
+            break
+        else
+            client.puts header
+            client.puts data
+        end
     end
+
     # end connection
     client.puts "Closing connection..."
     client.close
